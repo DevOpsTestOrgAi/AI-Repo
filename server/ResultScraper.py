8ba39263c26f17a663a7ba07e4a3eee639bbd682
Medelouali <Mohammedelouali1999@gmail.com>
2023-12-03 20:45:24 +0100

    Fix: Fixing the return format of the searcher from Jumia

--- server/ResultScraper.py
+++ server/ResultScraper.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup as Bs
 import csv
 import os
+from flask import Response, jsonify
 
 userInput = 'Knifes'
 
@@ -26,28 +27,21 @@ def res(userInput):
         response = requests.get(url)
         if response.status_code != 200:
             print(f"Error: {response.status_code}")
+            return Response(status=500)  # Return an appropriate error status
+
         soup = Bs(response.text, 'html.parser')
         cards = soup.find_all('article', 'prd _fb col c-prd')
-        #with open(userInput + '.csv', 'w', newline='', encoding='utf-8-sig') as f:
-        #    writer = csv.writer(f, delimiter=';')
 
-            # Write the header row once
-        #    writer.writerow(['Link'])
-
-        #    for card in cards:
-        #        record = get_record(card)
-        #        writer.writerow(['jumia.ma'+record])
-        #print(len(cards))  # check how many records are found
-        #os.startfile(userInput+'.csv')  # for Windows
-
+        cards__ = []
         for card in cards:
             record = get_record(card)
-            print('jumia.ma'+record)
-        print(len(cards))  # check how many records are found
+            cards__.append('jumia.ma' + record)
+            print('jumia.ma' + record)
 
-        return cards
+        return jsonify(cards__), 200
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+        return Response(status=500)  # Return an appropriate error status
 
 
 # res(userInput)
